24b42c10344bbc29b0e4e88c907269b01c1c68b0
Shnuer <SAU@SAU.ru>
2019-10-07 16:41:32 +0300

    little fix

--- testDB.py
+++ testDB.py
@@ -1,18 +1,19 @@
 import cpu_read
 
+type_variable_for_core = ' int'
+list_with_column_table_of_contents = []
 def commandCreateForMysql():
     """Function return Two Value's:
         
-        String with command for create table
-        
-        List with all num core"""
-        
+        1) String with command for create table
+        2) List with all num core"""
+
     #get list with value percent CPU 
     list_with_core = cpu_read.get_percentage_CPU()
 
     # constant for create string mysql command
     string_with_command_mysql = 'CREATE TABLE Employed(id int'
-    type_variable_for_core = ' int'
+    
 
     # a list in which there will be value with num core
     list_with_column_table_of_contents =[]
@@ -25,3 +26,15 @@ def commandCreateForMysql():
     string_with_command_mysql = string_with_command_mysql+')'
 
     return string_with_command_mysql, list_with_column_table_of_contents
+
+def commandAddDataToDB():
+    pass
+
+
+if __name__ == "__main__":
+
+    string_comand_create, list_with_num_core = commandCreateForMysql()
+
+    print(string_comand_create)
+    print()
+    print(list_with_num_core)
